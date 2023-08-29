0dae28ac0c57d6a7229c9170142f57fededc6370
infa-svali <118964763+infa-svali@users.noreply.github.com>
2023-08-29 11:53:45 +0530

    Update getReleaseData.go

--- utils/getReleaseData.go
+++ utils/getReleaseData.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-func ReleaseData(StartDate string, EndDate string, ids string, pod1 string, environment string) (map[string]config.Value, map[string]config.Value) {
+func ReleaseData(StartDate string, EndDate string, GlobalEnv string, AppEnv string) (map[string]config.Value, map[string]config.Value) {
 	perfusw1map := make(map[string]config.Value)
 	perfidsmap := make(map[string]config.Value)
 	listdetails := `{
@@ -142,72 +142,72 @@ func ReleaseData(StartDate string, EndDate string, ids string, pod1 string, envi
 		containernames = perfusw1.Containername
 		filepaths = perfusw1.Logfilepath
 		for _, apisofservices := range perfusw1.SchedulerService {
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, AppEnv)
 			SendESRequest(apisofservices, perfusw1map, query)
 		}
 		for _, apis := range perfusw1.KmsService {
-			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, AppEnv)
 			SendESRequest(apis, perfusw1map, query)
 		}
 		for _, apisofservices := range perfusw1.NotificationService {
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, AppEnv)
 			SendESRequest(apisofservices, perfusw1map, query)
 		}
 		for _, apis := range perfusw1.CAService {
-			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, AppEnv)
 			SendESRequest(apis, perfusw1map, query)
 		}
 		for _, apisofservices := range perfusw1.LicenseService {
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, AppEnv)
 			SendESRequest(apisofservices, perfusw1map, query)
 		}
 		for _, apis := range perfusw1.JlsService {
-			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, AppEnv)
 			SendESRequest(apis, perfusw1map, query)
 		}
 		for _, apisofservices := range perfusw1.BundleService {
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, AppEnv)
 			SendESRequest(apisofservices, perfusw1map, query)
 		}
-		
+
 		for _, apisofservices := range perfusw1.SessionService {
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, AppEnv)
 			SendESRequest(apisofservices, perfusw1map, query)
 		}
 		for _, apis := range perfusw1.Frs {
-			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, AppEnv)
 			SendESRequest(apis, perfusw1map, query)
 		}
 		for _, apisofservices := range perfusw1.AuditService {
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, AppEnv)
 			SendESRequest(apisofservices, perfusw1map, query)
 		}
 		for _, apis := range perfusw1.PreferenceService {
-			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, AppEnv)
 			SendESRequest(apis, perfusw1map, query)
 		}
 		for _, apis := range perfusw1.MigrationService {
-			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, AppEnv)
 			SendESRequest(apis, perfusw1map, query)
 		}
 		for _, apisofservices := range perfusw1.AdminService {
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, AppEnv)
 			SendESRequest(apisofservices, perfusw1map, query)
 		}
 		for _, apis := range perfusw1.Vcs {
-			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, AppEnv)
 			SendESRequest(apis, perfusw1map, query)
 		}
 		for _, apisofservices := range perfusw1.AcService {
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames, filepaths, AppEnv)
 			SendESRequest(apisofservices, perfusw1map, query)
 		}
 		for _, apis := range perfusw1.LdmService {
-			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, pod1)
+			query := buildmyquery(StartDate, EndDate, apis, containernames, filepaths, AppEnv)
 			SendESRequest(apis, perfusw1map, query)
 		}
 		for _, apis := range perfusw1.Runtime {
-			query := QueryRuntime(StartDate, EndDate, apis, filepaths, environment)
+			query := QueryRuntime(StartDate, EndDate, apis, filepaths, AppEnv)
 			SendESRequest(apis, perfusw1map, query)
 		}
 	}
@@ -220,36 +220,36 @@ func ReleaseData(StartDate string, EndDate string, ids string, pod1 string, envi
 		//log.Println(perfusw1, podname)
 		for _, apisofservices := range perfids.AuthService {
 			//log.Println(apisofservices, containernames, filepaths)
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, ids)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, GlobalEnv)
 			SendESRequest(apisofservices, perfidsmap, query)
 		}
 		for _, apisofservices := range perfids.BrandingService {
 			//log.Println(apisofservices, containernames, filepaths)
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, ids)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, GlobalEnv)
 			SendESRequest(apisofservices, perfidsmap, query)
 		}
 		for _, apisofservices := range perfids.ContentRepository {
 			//log.Println(apisofservices, containernames, filepaths)
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, ids)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, GlobalEnv)
 			SendESRequest(apisofservices, perfidsmap, query)
 		}
 		for _, apisofservices := range perfids.IdsService {
 			//log.Println(apisofservices, containernames, filepaths)
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, ids)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, GlobalEnv)
 			SendESRequest(apisofservices, perfidsmap, query)
 		}
 		for _, apisofservices := range perfids.MaService {
 			//log.Println(apisofservices, containernames, filepaths)
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, ids)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, GlobalEnv)
 			SendESRequest(apisofservices, perfidsmap, query)
 		}
 		for _, apisofservices := range perfids.ScimService {
 			//log.Println(apisofservices, containernames, filepaths)
-			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, ids)
+			query := buildmyquery(StartDate, EndDate, apisofservices, containernames2, filepaths2, GlobalEnv)
 			SendESRequest(apisofservices, perfidsmap, query)
 		}
 		for _, apisofservice := range perfids.V3API {
-			query := buildmyquery(StartDate, EndDate, apisofservice, containernames2, filepaths2, ids)
+			query := buildmyquery(StartDate, EndDate, apisofservice, containernames2, filepaths2, GlobalEnv)
 			SendESRequest(apisofservice, perfidsmap, query)
 		}
 	}
