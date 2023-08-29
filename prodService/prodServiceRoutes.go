d8773118b0587042d32c19e1c6bd51cd0a8548ae
infa-svali <118964763+infa-svali@users.noreply.github.com>
2023-08-29 14:05:21 +0530

    Update prodServiceRoutes.go

--- prodService/prodServiceRoutes.go
+++ prodService/prodServiceRoutes.go
@@ -5,6 +5,7 @@ import "net/http"
 func Init() {
 	http.HandleFunc("/test", test)
 	http.HandleFunc("/compare", compareResponseTimes)
+	http.HandleFunc("/EnvironmentDetails",displayenvdetails)
 	fileServer := http.FileServer(http.Dir("./htmlReports"))
 	http.Handle("/htmlReports/", http.StripPrefix("/htmlReports/", fileServer))
 }
